c0dba2b28793e181b66c1d6c78e1b14e5361e92c
Sasha-Gill <54946194+Sasha-Gill@users.noreply.github.com>
2020-05-25 13:18:44 +0200

    99% klar

--- FantasyRace/FantasyRace/Program.cs
+++ FantasyRace/FantasyRace/Program.cs
@@ -12,7 +12,7 @@ namespace FantasyRace
             string djur = Console.ReadLine();//det som skrivs in  blir en string
             int amount = 0;
 
-            while (!int.TryParse(djur, out amount))//en loop som körs så länge 
+            while (!int.TryParse(djur, out amount)&& amount <= 0)//en loop som körs så länge djur inte är ett nummer och amount är likamed eller större  än 0.
             {
                 Console.WriteLine("Try again. Please write a number over 0.");
                 djur = Console.ReadLine();
@@ -39,27 +39,29 @@ namespace FantasyRace
             Animal MainAnimal = animals[rng.Next(animals.Count)];//Ett djur slumpas från listan och blir Main animal.
             while (MainAnimal.position < 20)//så länge 20 är större än current pos så körs loopen
             {
+                Console.Clear();//den clearar allt som stått ovan, mest för att hålla tydligt o rent
                 string answer = "";//svaret på kommande fråga sparas i denna string
                 Console.WriteLine("Current round: "+Round);
                 MainAnimal.PrintStats();//Den visar djurets stats, print metoden
                 Console.WriteLine("Press 1 to move || Press 2 to rest");
 
-                while (answer!= "1" && answer!= "2")
+                while (answer!= "1" && answer!= "2")//om svaret inte är 1 eller 2 så sker detta
                 {
-                    answer = Console.ReadLine();
+                    answer = Console.ReadLine();//svaret nollställs och man får svara på frågan igen
                 }
                 if (answer == "1")
                 {
-                    MainAnimal.Move();
+                    MainAnimal.Move();//när man skriver in 1 så körs Move metoden
                 }
                 else if (answer == "2")
                 {
-                    MainAnimal.Rest();
+                    MainAnimal.Rest();//När am skriver in 2 körs Rest metoden
                 }
-                ++Round;
+                ++Round;// vid slutet av varje runda så ökar nummret.
             }
-
-            
+            Console.WriteLine("Congratultion you WON!");
+            Console.WriteLine("Press ENTER to exit game.");//när man vunnit kommer detta fram
+            Console.ReadLine();
         }
     }
 }
